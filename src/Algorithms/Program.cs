572ab1696a8fc52df8b5268c4479a7030e150353
Slawomir Was <was.slawek@gmail.com>
2026-02-02 11:11:17 +0100

    feat: add null-safe LinearSearch implementation
    Implemented LinearSearch extension method for IList using
    EqualityComparer<T>.Default to ensure null-safety during comparison.
    Method returns index and provides found element via out parameter.

--- src/Algorithms/Program.cs
+++ src/Algorithms/Program.cs
@@ -87,5 +87,25 @@
 
 			return collection;
 		}
+
+		public static int LinearSearch<T>(this IList<T> collection, T searchedValue, out T? result) where T : IEquatable<T> 
+		{
+			if(collection is null)
+			{
+				throw new ArgumentNullException(nameof(collection));
+			}
+
+			for (int i = 0; i < collection.Count; i ++)
+			{
+				if (EqualityComparer<T>.Default.Equals(collection[i], searchedValue))
+				{
+					result = collection[i];
+					return i;
+				}
+			}
+
+			result = default;
+			return -1;
+		}
 	}
 }
